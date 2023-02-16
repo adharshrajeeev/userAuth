b195d64c45305b9825132b94e62eec1168ecd065
Adharsh R <adharshrajeeev2000@gmail.com>
2023-02-16 17:30:05 +0530

    homePage added

---
+++ frontend/src/Components/Headers/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './NavBarStyle.css'
+
+function NavBar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg"  >
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">Navbar</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          </div>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default NavBar
--- frontend/src/Components/HomePage/HomeScreen.jsx
+++ frontend/src/Components/HomePage/HomeScreen.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './HomeStyle.css'
 
 function HomeScreen() {
+  let [count,setCount]=useState(0);
+
+  function handleClick(){
+  setCount(count + 1);
+  console.log(count)
+  }
   return (
-    <div>HomeScreen</div>
+    <div class="container">
+
+
+    <div class="row container1">
+    <div class="col-sm-6">
+      <div class="card homeCard1" >
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+          <p>{count}</p>
+          <button  onClick={handleClick} class="btn btn-primary">Count</button> 
+          <button onClick={()=>setCount(0)} class="btn btn-primary">RESET</button>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+  </div>
   )
 }
 
