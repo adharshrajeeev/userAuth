aff39f04977f88fd08a798b2598e802197d00ec9
Adharsh R <adharshrajeeev2000@gmail.com>
2023-04-12 20:49:42 +0530

    changed added env and changed db to atlas for hosting

--- backend/app.js
+++ backend/app.js
@@ -3,27 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 const cors=require('cors')
+const dotenv=require('dotenv');
 const mongoose=require('mongoose')
 // var logger = require('morgan');
 
 
 var usersRouter = require('./routes/users');
 
+
 var app = express();
 
 
 // app.use(logger('dev'));
-
+dotenv.config();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-mongoose.connect('mongodb://localhost:27017/userAuth')
 
 app.use('/', usersRouter);
 
-app.listen(3000,()=>console.log("POST RUNNNIT IN 3000"))
+mongoose.connect(process.env.DATABASE).then((response)=>{
+
+    app.listen(process.env.PORT,()=>console.log(`APP RUNNING IN PORT ${process.env.PORT}`))
+}).catch((err)=>{
+    console.log("error in connections")
+})
 
 module.exports = app;
